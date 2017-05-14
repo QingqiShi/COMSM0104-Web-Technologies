d8a02630769824fd42f1d3d7707ed3e415bcb407
Qingqi Shi <qingqishi@live.com>
2017-05-14 13:49:54 +0100

    Created SQLite database

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('data.db')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Game of Life - Home' });
