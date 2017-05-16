f35d534a554eb15e19660e15ec22dfa813c4180c
Qingqi Shi <qingqishi@live.com>
2017-05-16 16:32:44 +0100

    Added sign in functionalities
    Although, cookies haven't been added yet

--- express/public/javascripts/sign_in_sign_up.js
+++ express/public/javascripts/sign_in_sign_up.js
@@ -26,6 +26,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
+    var sign_in_result = getQueryString("sign_in_result");
+    switch (sign_in_result) {
+        case "FAILED_ERROR":
+            show_class("back_drop");
+            show_class("sign_in_pop_up");
+            show_class("sign_in_fail");
+            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: error with databse";
+            break;
+        case "FAILED_NO_USER":
+            show_class("back_drop");
+            show_class("sign_in_pop_up");
+            show_class("sign_in_fail");
+            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: user doesn't exist";
+            break;
+        case "FAILED_INCORRECT_PASSWORD":
+            show_class("back_drop");
+            show_class("sign_in_pop_up");
+            show_class("sign_in_fail");
+            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: password wrong";
+            break;
+        case "SUCCESS":
+            break;
+    }
+
     var sign_in_btn = document.getElementById("user_sign_in_btn");
     var sign_in_close_btn = document.querySelectorAll(".sign_in_pop_up .close")[0];
 
@@ -51,6 +75,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     sign_up_close_btn.addEventListener("click", function(){
         hide_class("back_drop");
         hide_class("sign_up_pop_up");
+    });
+
+    var sign_up_from_sign_in = document.querySelectorAll(".sign_up_message a")[0];
+
+    sign_up_from_sign_in.addEventListener("click", function() {
+        hide_class("back_drop");
+        hide_class("sign_in_pop_up");
+        show_class("back_drop");
+        show_class("sign_up_pop_up");
     });
 });
 
--- express/routes/signin.js
+++ express/routes/signin.js
@@ -1,14 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-var sqlite3 = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data.db');
+
+var crypto = require('crypto');
 
 /* Post sign in. */
 router.post('/', function(req, res, next) {
     var url = encodeURIComponent(req.body.url);
-    console.log(url);
-    res.redirect(url);
+    if (url === null) {
+        url = "index.html";
+    }
+    var user_name = req.body.user_name;
+    var password = req.body.password;
+
+    db.get("SELECT * FROM Users WHERE user_name=?", user_name, function(err, row) {
+        var result = "";
+        if (err !== null) {
+            result = "FAILED_ERROR";
+        } else if (typeof row == 'undefined') {
+            result = "FAILED_NO_USER"
+        } else {
+            var salt = row.user_salt;
+            var hash = crypto.pbkdf2Sync(password, salt, 100000, 512, 'sha512').toString('hex');
+            if (hash == row.user_hash) {
+                result = "SUCCESS";
+            } else {
+                result = "FAILED_INCORRECT_PASSWORD";
+            }
+        }
+
+        res.redirect(url + "?sign_in_result=" + result);
+    });
+
 });
 
 module.exports = router;
