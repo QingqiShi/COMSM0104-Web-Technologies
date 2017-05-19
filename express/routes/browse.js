040ff3702e12a1ed894ef78ce0d33830df226eda
Qingqi Shi <qingqishi@live.com>
2017-05-19 18:00:42 +0100

    Reversed browse page display order
    Now newest games are on top

--- express/routes/browse.js
+++ express/routes/browse.js
@@ -7,9 +7,14 @@ var db = new sqlite3.Database('data.db');
 router.get('/', function(req, res, next) {
     db.all("SELECT * FROM Game_States", function(err, rows) {
         if (err !== null) {
-            var result = "DB_ERROR";
-            res.redirect("game.html?browse_result=" + result);
+            if (req.session.user_name) {
+                res.render('browse', { title: 'Game of Life - Browse', game_states: [], user_name: req.session.user_name });
+            } else {
+                res.render('browse', { title: 'Game of Life - Browse', game_states: [] });
+            }
         } else {
+
+            rows = rows.reverse();
             if (req.session.user_name) {
                 res.render('browse', { title: 'Game of Life - Browse', game_states: rows, user_name: req.session.user_name });
             } else {
