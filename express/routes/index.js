ea1d631a20284c7553ade0d2e104390984f9587e
Qingqi Shi <qingqishi@live.com>
2017-05-23 19:52:31 +0100

    Added background image to home page site name sec.

--- express/routes/index.js
+++ express/routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if (req.session.user_name) {
-        res.render('index', { title: 'Game of Life - Home', user_name: req.session.user_name });
+        res.render('index', { title: 'Game of Life - Home', home: true, user_name: req.session.user_name });
     } else {
-        res.render('index', { title: 'Game of Life - Home' });
+        res.render('index', { title: 'Game of Life - Home', home: true });
     }
 });
 
