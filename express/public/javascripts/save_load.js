d30efa2122ac845744d5b92f9b84ed94c7dda7d3
Qingqi Shi <qingqishi@live.com>
2017-05-25 02:36:39 +0100

    Added client-side validation and refactored code

--- express/public/javascripts/save_load.js
+++ express/public/javascripts/save_load.js
@@ -7,72 +7,30 @@ var game_convert = function() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    var publish_result = getQueryString("publish_result");
-    if (publish_result !== null) {
-        show_class("back_drop");
+    var sign_in_btn = document.getElementById("user_sign_in_btn");
+    var sign_in_from_game = document.querySelectorAll(".game_info .sign_in")[0];
+    var sign_up_btn = document.getElementById("user_sign_up_btn");
+    var publish_btn = document.querySelectorAll(".game_info .publish")[0];
 
-        switch (publish_result) {
-            case "EMPTY_TITLE":
-                show_class("publish_pop_up");
-                show_class("publish_fail");
-                document.getElementsByClassName("publish_fail")[0].innerHTML = "Publish failed: title cannot be empty";
-                break;
-            case "EMPTY_DESCRIPTION":
-                show_class("publish_pop_up");
-                show_class("publish_fail");
-                document.getElementsByClassName("publish_fail")[0].innerHTML = "Publish failed: description must not be empty";
-                break;
-            case "DATA_INVALID":
-                show_class("publish_pop_up");
-                show_class("publish_fail");
-                document.getElementsByClassName("publish_fail")[0].innerHTML = "Publish failed: invalid game data";
-                break;
-            case "FAILED":
-                show_class("publish_pop_up");
-                show_class("publish_fail");
-                document.getElementsByClassName("publish_fail")[0].innerHTML = "Publish failed: database error";
-                break;
-            case "SUCCESS":
-                hide_class("back_drop");
-                break;
+    var game_data_field = document.getElementById("game_data");
+
+    var register_pause = function(btn) {
+        if (typeof btn != 'undefined' && btn) {
+            btn.addEventListener("click", function(){
+                game_pause();
+            });
         }
     }
 
+    register_pause(sign_in_btn);
+    register_pause(sign_in_from_game);
+    register_pause(sign_up_btn);
+    register_pause(publish_btn);
 
-    var sign_in_btn = document.querySelectorAll(".game_info .sign_in")[0];
-    var sign_in_close_btn = document.querySelectorAll(".sign_in_pop_up .close")[0];
-
-    if (sign_in_btn !== null && typeof sign_in_btn != 'undefined') {
-        sign_in_btn.addEventListener("click", function(){
-            game_pause();
-            show_class("back_drop");
-            show_class("sign_in_pop_up");
-        });
-    }
-
-    sign_in_close_btn.addEventListener("click", function(){
-        hide_class("back_drop");
-        hide_class("sign_in_pop_up");
-    });
-
-    var publish_btn = document.querySelectorAll(".game_info .publish")[0];
-    var publish_close_btn = document.querySelectorAll(".publish_pop_up .close")[0];
-
-    if (publish_btn !== null && typeof publish_btn != 'undefined') {
+    if (typeof publish_btn != 'undefined' && publish_btn) {
         publish_btn.addEventListener("click", function(){
-            game_pause();
             var data = game_convert();
-
-            var game_data_field = document.getElementById("game_data");
             game_data_field.setAttribute("value", data);
-
-            show_class("back_drop");
-            show_class("publish_pop_up");
         });
     }
-
-    publish_close_btn.addEventListener("click", function(){
-        hide_class("back_drop");
-        hide_class("publish_pop_up");
-    });
 });
--- express/public/javascripts/sign_in_sign_up.js
+++ express/public/javascripts/sign_in_sign_up.js
@@ -7,123 +7,247 @@ document.addEventListener("DOMContentLoaded", function(event) {
         duration: 300,
     });
 
+    /* pop_up back drop */
+    var back_drop = document.getElementsByClassName("back_drop")[0];
+
+    /* Sign up elements */
     var sign_up_result = getQueryString("sign_up_result");
-    if (sign_up_result !== null) {
-        show_class("back_drop");
+    var sign_up_btn = document.getElementById("user_sign_up_btn");
+    var sign_up_from_sign_in = document.querySelectorAll(".sign_up_message a")[0];
+    var sign_up_pane = document.getElementsByClassName("sign_up_pop_up")[0];
+    var sign_up_form = document.querySelectorAll(".sign_up_pop_up form")[0];
+    var sign_up_fail_pane = document.getElementsByClassName("sign_up_fail")[0];
+    var sign_up_close_btn = document.querySelectorAll(".sign_up_pop_up .close")[0];
+    var sign_up_user_name = document.getElementById("sign_up_user_name");
+    var sign_up_password = document.getElementById("sign_up_password");
+    var repeat_password = document.getElementById("repeat_password");
+    var sign_up_submit = document.querySelectorAll(".sign_up_pop_up .btn")[0];
+
+    /* Sign in elements */
+    var sign_in_result = getQueryString("sign_in_result");
+    var sign_in_btn = document.getElementById("user_sign_in_btn");
+    var sign_in_from_game = document.querySelectorAll(".game_info .sign_in")[0];
+    var sign_in_pane = document.getElementsByClassName("sign_in_pop_up")[0];
+    var sign_in_form = document.querySelectorAll(".sign_in_pop_up form")[0];
+    var sign_in_success_pane = document.getElementsByClassName("sign_in_success")[0];
+    var sign_in_fail_pane = document.getElementsByClassName("sign_in_fail")[0];
+    var sign_in_close_btn = document.querySelectorAll(".sign_in_pop_up .close")[0];
+    var sign_in_user_name = document.getElementById("sign_in_user_name");
+    var sign_in_password = document.getElementById("sign_in_password");
+    var sign_in_submit = document.querySelectorAll(".sign_in_pop_up .btn")[0];
+
+    /* Publish elements */
+    var publish_result = getQueryString("publish_result");
+    var publish_pane = document.getElementsByClassName("publish_pop_up")[0];
+    var publish_btn = document.querySelectorAll(".game_info .publish")[0];
+    var publish_close_btn = document.querySelectorAll(".publish_pop_up .close")[0];
+    var publish_form = document.querySelectorAll(".publish_pop_up form")[0];
+    var publish_fail_pane = document.getElementsByClassName("publish_fail")[0];
+    var publish_title = document.getElementById("publish_title");
+    var publish_description = document.getElementById("publish_description");
+    var publish_submit = document.querySelectorAll(".publish_pop_up .btn")[0];
+
+
+    /* Show and hide elements */
+    var show_element = function(element) {
+        if (element.classList.contains("hide")) {
+            element.classList.remove("hide");
+        }
+    };
+
+    var hide_element = function(element) {
+        if (!element.classList.contains("hide")) {
+            element.classList.add("hide");
+        }
+    };
+
+
+    /* Initialise panes */
+    var initialise_pane = function(open_btn, close_btn, pane) {
+        if (open_btn) {
+            open_btn.addEventListener("click", function() {
+                show_element(back_drop);
+                show_element(pane);
+            });
+        }
+
+        if (close_btn) {
+            close_btn.addEventListener("click", function() {
+                hide_element(back_drop);
+                hide_element(pane);
+            });
+        }
+    };
+
+
+    /* Show message */
+    var show_message = function(message_pane, message) {
+        show_element(message_pane);
+        message_pane.innerHTML = message;
+    }
+
+
+    /* Validate functions */
+    var validate_sign_up = function() {
+        if (sign_up_user_name.value.length < 5) {
+            show_message(sign_up_fail_pane, "Username too short (at least 5 characters)");
+            return false;
+        } else if (sign_up_password.value.length < 10) {
+            show_message(sign_up_fail_pane, "Password too short (at least 10 characters)");
+            return false;
+        } else if (repeat_password.value != sign_up_password.value) {
+            show_message(sign_up_fail_pane, "Repeated password not the same");
+            return false;
+        } else {
+            hide_element(sign_up_fail_pane);
+            return true;
+        }
+    }
+
+    var validate_sign_in = function() {
+        if (sign_in_user_name.value.length < 1) {
+            show_message(sign_in_fail_pane, "Username can't be empty");
+            return false;
+        } else if (sign_in_password.value.length < 1) {
+            show_message(sign_in_fail_pane, "Password can't be empty");
+            return false;
+        } else {
+            hide_element(sign_in_fail_pane);
+            return true;
+        }
+    }
+
+    var validate_publish = function() {
+        if (publish_title.value.length < 1) {
+            show_message(publish_fail_pane, "Title can't be empty");
+            return false;
+        } else if (publish_description.value.length < 1) {
+            show_message(publish_fail_pane, "Description can't be empty");
+            return false;
+        } else {
+            hide_element(publish_fail_pane);
+            return true;
+        }
+    }
+
+
+    /* Initialise submit buttons */
+    var initialise_submit = function(btn, form, validator) {
+        if (btn) {
+            btn.addEventListener("click", function(){
+                if (typeof game_controller != 'undefined' && game_controller) {
+                    save_local();
+                }
+
+                if (validator()) {
+                    form.submit();
+                }
+            });
+        }
+    }
+
+
+    /* Panes */
+    initialise_pane(sign_up_btn, sign_up_close_btn, sign_up_pane);
+    initialise_pane(sign_in_btn, sign_in_close_btn, sign_in_pane);
+    initialise_pane(sign_in_from_game,  null, sign_in_pane);
+    initialise_pane(publish_btn, publish_close_btn, publish_pane);
+    initialise_pane(null, sign_up_from_sign_in, sign_in_pane);
+    initialise_pane(sign_up_from_sign_in, null, sign_up_pane);
+
+
+    /* Submits */
+    initialise_submit(sign_up_submit, sign_up_form, validate_sign_up);
+    initialise_submit(sign_in_submit, sign_in_form, validate_sign_in);
+    initialise_submit(publish_submit, publish_form, validate_publish);
+
+
+    /* Database errors */
+    if (sign_up_result) {
+        show_element(back_drop);
 
         switch (sign_up_result) {
             case "USER_NAME_TOO_SHORT":
-                show_class("sign_up_pop_up");
-                show_class("sign_up_fail");
-                document.getElementsByClassName("sign_up_fail")[0].innerHTML = "Sign up failed: username too short (at least 5 characters)";
+                show_element(sign_up_pane);
+                show_element(sign_up_fail_pane);
+                sign_up_fail_pane.innerHTML = "Sign up failed: username too short (at least 5 characters)";
                 break;
             case "PASSWORD_TOO_SHORT":
-                show_class("sign_up_pop_up");
-                show_class("sign_up_fail");
-                document.getElementsByClassName("sign_up_fail")[0].innerHTML = "Sign up failed: password too short (at least 10 characters)";
+                show_element(sign_up_pane);
+                show_element(sign_up_fail_pane);
+                sign_up_fail_pane.innerHTML = "Sign up failed: password too short (at least 10 characters)";
                 break;
             case "FAILED":
-                show_class("sign_up_pop_up");
-                show_class("sign_up_fail");
-                document.getElementsByClassName("sign_up_fail")[0].innerHTML = "Sign up failed: username was chosen";
+                show_element(sign_up_pane);
+                show_element(sign_up_fail_pane);
+                sign_up_fail_pane.innerHTML = "Sign up failed: username was chosen";
                 break;
             case "SUCCESS":
-                show_class("sign_in_pop_up");
-                show_class("sign_in_success");
-                document.getElementsByClassName("sign_in_success")[0].innerHTML = "Sign up successful, please sign in";
+                show_element(sign_in_pane);
+                show_element(sign_in_success_pane);
+                sign_in_success_pane.innerHTML = "Sign up successful, please sign in";
                 break;
         }
     }
 
-    var sign_in_result = getQueryString("sign_in_result");
-    switch (sign_in_result) {
-        case "FAILED_ERROR":
-            show_class("back_drop");
-            show_class("sign_in_pop_up");
-            show_class("sign_in_fail");
-            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: error with databse";
-            break;
-        case "FAILED_NO_USER":
-            show_class("back_drop");
-            show_class("sign_in_pop_up");
-            show_class("sign_in_fail");
-            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: user doesn't exist";
-            break;
-        case "FAILED_INCORRECT_PASSWORD":
-            show_class("back_drop");
-            show_class("sign_in_pop_up");
-            show_class("sign_in_fail");
-            document.getElementsByClassName("sign_in_fail")[0].innerHTML = "Sign in failed: password wrong";
-            break;
-        case "SUCCESS":
-            break;
+    if (sign_in_result) {
+        switch (sign_in_result) {
+            case "FAILED_ERROR":
+                show_element(back_drop);
+                show_element(sign_in_pane);
+                show_element(sign_in_fail_pane);
+                sign_in_fail_pane.innerHTML = "Sign in failed: error with databse";
+                break;
+            case "FAILED_NO_USER":
+                show_element(back_drop);
+                show_element(sign_in_pane);
+                show_element(sign_in_fail_pane);
+                sign_in_fail_pane.innerHTML = "Sign in failed: user doesn't exist";
+                break;
+            case "FAILED_INCORRECT_PASSWORD":
+                show_element(back_drop);
+                show_element(sign_in_pane);
+                show_element(sign_in_fail_pane);
+                sign_in_fail_pane.innerHTML = "Sign in failed: password wrong";
+                break;
+            case "SUCCESS":
+                break;
+        }
     }
 
-    var sign_in_btn = document.getElementById("user_sign_in_btn");
-    var sign_in_close_btn = document.querySelectorAll(".sign_in_pop_up .close")[0];
+    if (publish_result) {
+        show_element(back_drop);
 
-    if (sign_in_btn !== null) {
-        sign_in_btn.addEventListener("click", function(){
-            show_class("back_drop");
-            show_class("sign_in_pop_up");
-        });
+        switch (publish_result) {
+            case "EMPTY_TITLE":
+                show_element(publish_pane);
+                show_element(publish_fail_pane);
+                publish_fail_pane.innerHTML = "Publish failed: title cannot be empty";
+                break;
+            case "EMPTY_DESCRIPTION":
+                show_element(publish_pane);
+                show_element(publish_fail_pane);
+                publish_fail_pane.innerHTML = "Publish failed: description must not be empty";
+                break;
+            case "DATA_INVALID":
+                show_element(publish_pane);
+                show_element(publish_fail_pane);
+                publish_fail_pane.innerHTML = "Publish failed: invalid game data";
+                break;
+            case "FAILED":
+                show_element(publish_pane);
+                show_element(publish_fail_pane);
+                publish_fail_pane.innerHTML = "Publish failed: database error";
+                break;
+            case "SUCCESS":
+                hide_element(back_drop);
+                break;
+        }
     }
 
-    sign_in_close_btn.addEventListener("click", function(){
-        hide_class("back_drop");
-        hide_class("sign_in_pop_up");
-    });
-
-
-    var sign_up_btn = document.getElementById("user_sign_up_btn");
-    var sign_up_close_btn = document.querySelectorAll(".sign_up_pop_up .close")[0];
-
-    if (sign_up_btn !== null) {
-        sign_up_btn.addEventListener("click", function(){
-            show_class("back_drop");
-            show_class("sign_up_pop_up");
-        });
-    }
-
-    sign_up_close_btn.addEventListener("click", function(){
-        hide_class("back_drop");
-        hide_class("sign_up_pop_up");
-    });
-
-    var sign_up_from_sign_in = document.querySelectorAll(".sign_up_message a")[0];
-
-    sign_up_from_sign_in.addEventListener("click", function() {
-        hide_class("back_drop");
-        hide_class("sign_in_pop_up");
-        show_class("back_drop");
-        show_class("sign_up_pop_up");
-    });
-
-    var publish_form = document.querySelectorAll(".publish_pop_up form")[0];
-    var sign_in_form = document.querySelectorAll(".sign_in_pop_up form")[0];
-    var sign_up_form = document.querySelectorAll(".sign_up_pop_up form")[0];
-    var publish_submit = document.querySelectorAll(".publish_pop_up .btn")[0];
-    var sign_in_submit = document.querySelectorAll(".sign_in_pop_up .btn")[0];
-    var sign_up_submit = document.querySelectorAll(".sign_up_pop_up .btn")[0];
-    publish_submit.addEventListener("click", function(){
-        if (typeof game_controller !== 'undefined') {
-            save_local();
-        }
-        publish_form.submit();
-    });
-    sign_in_submit.addEventListener("click", function(){
-        if (typeof game_controller !== 'undefined') {
-            save_local();
-        }
-        sign_in_form.submit();
-    });
-    sign_up_submit.addEventListener("click", function(){
-        if (typeof game_controller !== 'undefined') {
-            save_local();
-        }
-        sign_up_form.submit();
-    });
 });
+
+
 
 var hide_class = function(class_name) {
     var back_drop = document.getElementsByClassName(class_name)[0];
